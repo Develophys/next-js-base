e66b871ca57c8fcc814b9f6ced2a4e6de284f8b1
Mauricio AB <dev.mauricioalezandre@gmail.com>
2024-05-05 19:49:50 -0300

    ✨ feat: adding header

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,9 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
-
 export default function HomePage() {
   return (
     <>
-      <h1>HOME PAGE</h1>
-      <Button onClick={() => signOut()}>Sign OUT</Button>
+      <div className="flex justify-center">
+        <h1>HOME PAGE</h1>
+      </div>
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,62 @@
+"use client";
+import Link from "next/link";
+
+import { signOut } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "./ui/menubar";
+
+export function Header() {
+  return (
+    <div className="container mx-auto px-4 md:px-6 lg:px-8">
+      <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+        <div className="ml-auto flex gap-2">
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href="/home"
+          >
+            Home
+          </Link>
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href="/home"
+          >
+            Classes
+          </Link>
+
+          <Menubar className="border-none">
+            <MenubarMenu>
+              <MenubarTrigger>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem disabled>Profile</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem disabled>Classes</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem
+                  className="cursor-pointer"
+                  onClick={() => signOut()}
+                >
+                  Sign OUT
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </div>
+      </header>
+    </div>
+  );
+}
